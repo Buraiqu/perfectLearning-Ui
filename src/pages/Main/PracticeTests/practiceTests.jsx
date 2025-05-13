ddeccda546b7c82f3d0011559aacef654aaa2173
Athul krishna v <athulkrishnav@Athuls-MacBook-Air.local>
2025-05-13 12:57:48 +0530

    phase-5 first commit

---
+++ src/pages/Main/PracticeTests/practiceTests.jsx
@@ -0,0 +1,13 @@
+import './practiceTests.css'
+
+const PracticeTests = () => {
+
+    return (
+        <div className="practice-tests-container">
+            <div className="box1"></div>
+            <div className="box1"></div>
+        </div>
+    )
+}
+
+export default PracticeTests
