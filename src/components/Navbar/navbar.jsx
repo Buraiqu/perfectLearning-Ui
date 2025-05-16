956e944e11348da2ca3ab99a157e46fded2fdb97
Athul krishna v <athulkrishnav@Athuls-MacBook-Air.local>
2025-05-16 17:10:02 +0530

    added search icon functionality

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -14,10 +14,22 @@ const CustomNavbar = ({user}) => {
     const [expanded, setExpanded] = useState(false);
     const { width } = useWindowSize();
     const isMobile = width <= 1130;
-    const { searchQuery, setSearchQuery } = useSearch();
+    const { setSearchQuery } = useSearch();
+    const [ handleSearch, setHandleSearch ] = useState('');
 
     const handleSearchChange = (e) => {
-        setSearchQuery(e.target.value);
+        setHandleSearch(e.target.value);
+    };
+
+    const handleClickSearch = () => {
+        setSearchQuery(handleSearch);
+    };
+    
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            handleClickSearch();
+        }
     };
 
     useEffect(() => {
@@ -68,12 +80,13 @@ const CustomNavbar = ({user}) => {
                             <Form.Control
                                 type="search"
                                 placeholder="Search"
-                                className="search-input"
+                                className="search-input no-clear-button"
                                 aria-label="Search"
-                                value={searchQuery}
+                                value={handleSearch}
                                 onChange={handleSearchChange}
+                                onKeyPress={handleKeyPress}
                             />
-                            <div className="search-icon">
+                            <div className="search-icon" onClick={handleClickSearch}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                                 </svg>
